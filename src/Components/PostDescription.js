1e26fb553c95c890df7f2281e7902daf9693433f
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-09-21 12:09:14 -0400

    Finally figured out how to get Username on the same line as paragraph

--- src/Components/PostDescription.js
+++ src/Components/PostDescription.js
@@ -1,12 +1,10 @@
 import Username from "./Username"
 
 const PostDescription = () => {
-
     return (
         <div className="postDescriptionDiv">
             <div className="postDescription">
-                <Username/>
-                <p>Hello, my name is Brenden Bass, and welcome to my Ted Talk.</p>
+                <p className="postText"> <Username/> Hello, my name is Brenden Bass, and welcome to my Ted Talk.</p>
             </div>
         </div>
     )
