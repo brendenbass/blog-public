d46e4e17ccbac071311377f2a5108869b4a79c66
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-09-23 10:20:53 -0400

    Fixed date

--- src/Components/PostTime.js
+++ src/Components/PostTime.js
@@ -1,28 +1,24 @@
 const PostTime = ({ created }) => {
-    // console.log('created', {created});
-    // const currentDate = (created);
-    const isoDate = created;
-    const trimmedDate = isoDate.split("T",[0]);
-    console.log(trimmedDate);
 
+    //cuts off extra time info
+    const normalDate = created.substring(0,10);
 
+    //grabs Year
+    const currentYear = normalDate.substring(0,4);
 
+    //grabs Month and deletes 0 if it is there
+    const currentMonth = normalDate.substring(5,7);
+    const noZerosMonth = currentMonth.replace('0','');
 
-    // console.log('readableDate', read);
-    // const currentDay = currentDate;
-    // // console.log('currentDay', currentDay);
-    // const currentMonth = currentDate;
-    // // console.log('currentMonth', currentMonth);
-    // const currentYear = currentDate;
-    // // console.log('currentYear', currentYear);
+    //grabs Day
+    const currentDay = normalDate.substring(8,10);
 
-    // const months = [ "", "January", "February", "March", "April", "May", "June", 
-    //        "July", "August", "September", "October", "November", "December" ];
+    const months = [ "", "January", "February", "March", "April", "May", "June", 
+           "July", "August", "September", "October", "November", "December" ];
 
-    // const selectedMonthName = months[currentMonth];
+    const selectedMonthName = months[noZerosMonth];
 
-    // const dateString = `${selectedMonthName} ${currentDay}, ${currentYear}`;
-    const dateString = created;
+    const dateString = `${selectedMonthName} ${currentDay}, ${currentYear}`;
     return (
         <div className="postTime">
             {dateString}
